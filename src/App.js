e78c3a37d4d125a39ce3e25890eb5fffdacc845e
Student <example@example.com>
2019-09-17 13:53:29 -0700

    It shows how to use key-value pair data in react

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import Tweet from './tweet';
 
 function App() {
 
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  };
+  const [users, setUsers] = useState([
+    {name: 'Ed', message: 'Hello there'},
+    {name: 'John', message: 'I am John Snow'},
+    {name: 'traversy', message: 'Awesome React'}
+  ]);
 
   return(
     <div className="app">
-      <h1 className={isRed ? 'red' : ''}>Change my color</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
