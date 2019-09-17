2b332a013b3617f59d2b3710754f39b239843f5d
Student <example@example.com>
2019-09-17 13:06:07 -0700

    It says hello with onClick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 function App() {
+
+  const sayHello = () => {console.log("hello");}
+  // const counter = 0;
+
   return(
     <div>
       <h1>Hello React</h1>
+      <button onClick={sayHello}>Hello</button>
     </div>
-  )
+  );
 }
 
 export default App;
